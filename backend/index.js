3faaf20cfa8402db88784580f240eac7c0d5ea06
Rounak_Biswal <rounakbiswal2003@gmail.com>
2026-04-07 01:42:21 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const cors = require('cors')
 const port = 8100
 const app = express()
 
-app.use(express.cors())
+app.use(cors())
 app.use(express.json())
 
-app.listen(port, ()=>{
-    console.log('server live at ',port );
+app.listen(port, () => {
+    console.log('server live at ', port);
 })
