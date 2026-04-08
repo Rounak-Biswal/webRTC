c25ef18666869a8b54c40183ca05d3901981bb18
Rounak_Biswal <rounakbiswal2003@gmail.com>
2026-04-09 02:37:08 +0530

    feat: join and online-user list implemente

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,29 @@ const { Server } = require("socket.io")
 const port = 8100
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
+
+const joinedUsers = {}
 
 io.on("connection", (socket) => {
     console.log('socket connected : ', socket.id);
 
     socket.on("join", (username) => {
-        console.log(`username associated with current socket : ${username}`);
+        if (username) {
+            console.log(`username associated with current socket : ${username}`);
+            joinedUsers[socket.id] = username
+            console.log('joinedUsers:', joinedUsers);
+            socket.emit("success", "user sucessfully joined")
+            io.emit("online-users", joinedUsers)
+        }
     })
 })
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { socket } from "./utils/socket"
 import './App.css'
 
 function App() {
   const [username, setUsername] = useState('')
+  const [onlineUsers, SetOnlineUsers] = useState([])
+  const [selectedUser, setSelectedUser] = useState('')
+
+  useEffect(() => {
+    socket.on("success", (msg) => {
+      console.log(msg)
+    })
+    socket.on("online-users", (userDB) => {
+      let arr = []
+      for (let key in userDB) { arr.push(userDB[key]) }
+      SetOnlineUsers(arr)
+    })
+    return () => {
+      socket.off("success")
+      socket.off("online-users")
+    }
+  }, [])
 
   let joinUser = () => {
     console.log(username)
@@ -21,7 +38,19 @@ function App() {
           id="usernameInput"
           onChange={(e) => setUsername(e.target.value)}
         />
-        <button onClick={joinUser}>Join</button>
+        <button onClick={joinUser}>Join</button><hr />
+
+        <h4>Online Users</h4>
+        {onlineUsers.map((username, index) => {
+          return <button
+            key={index}
+            onClick={() => {
+              setSelectedUser(username)
+              console.log("selected user : ", username)
+            }}>
+            {username}
+          </button>
+        })}
       </section>
     </>
   )
